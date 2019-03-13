d9147d7a4552857e6d84646ca3010a33c99ae570
Alex Merecka <merecka@gmail.com>
2019-03-13 04:57:19 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,7 +9,7 @@ def second_challenge
       }, 
     :capulet => {
       #your key/value pairs here!
-      :patriach => {},
+      :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
