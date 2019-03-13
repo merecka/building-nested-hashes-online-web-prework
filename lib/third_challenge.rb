619caac31423f8f1e7eba4bc8a9fde548fea1d86
Alex Merecka <merecka@gmail.com>
2019-03-13 05:09:25 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,6 +13,9 @@ def third_challenge
         },
       :hero => {
         #your key/value pairs here
+        :name => "Romeo",
+        :age => "15"
+        
         },
       :hero_friends => []
    }, 
